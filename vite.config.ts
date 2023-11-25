2c0f0b8494d1ad3850db28b0d5a12c64ff854e6b
anngreen <anngreen@qq.com>
2023-11-26 01:30:41 +0800

    blogs

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  publicDir:'./',
-  build:{
-    outDir:'docs'
+  base: '/blogs/',
+  build: {
+    outDir: 'docs'
   }
 })
