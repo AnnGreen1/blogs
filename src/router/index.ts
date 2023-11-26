f12f988b07d3906fc84b113931378de4b85e337e
anngreen <anngreen@qq.com>
2023-11-26 12:53:04 +0800

    添加elementplus

---
+++ src/router/index.ts
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory, createWebHashHistory, RouteRecordRaw } from "vue-router";
+
+
+const commonRoute: RouteRecordRaw[] = [
+    // commonroute
+    {
+        path: "/commonroute",
+        name: "commonroute",
+        component: () => import("@/views/CommonRoute.vue")
+    },
+    {
+        path: "/elementplusdemo",
+        name: "elementplusdemo",
+        component: () => import("@/views/ElementplusDemo.vue")
+    },
+];
+
+
+const router = createRouter({
+    // createWebHashHistory:hash模式; createWebHistory:历史模式; createMemoryHistory:ssr
+    history: createWebHashHistory(),
+    routes: [
+        ...commonRoute
+    ] as unknown as RouteRecordRaw[]
+});
+export default router
